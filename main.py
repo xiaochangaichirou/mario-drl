bb12ef81896c8a3f713594ebb8fc9f3afeafbaef
xiaochangaichirou <1207789049@qq.com>
2023-11-22 16:33:33 +0800

    On branch master
     Initial commit
    
     Changes to be committed:
    	new file:   .idea/.gitignore
    	new file:   .idea/inspectionProfiles/profiles_settings.xml
    	new file:   .idea/mario.iml
    	new file:   .idea/misc.xml
    	new file:   .idea/modules.xml
    	new file:   main.py
    	new file:   memeory.py
    	new file:   net.py
    	new file:   utils.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from nes_py.wrappers import JoypadSpace
+import gym_super_mario_bros
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+import time
+
+env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0')
+env = JoypadSpace(env, SIMPLE_MOVEMENT)
+
+done = True
+for step in range(5000):
+    if done:
+        state = env.reset()
+    state, reward, done, info = env.step(env.action_space.sample())
+    time.sleep(0.01)
+    env.render()
+
+env.close()
---
+++ net.py
@@ -0,0 +1,11 @@
+import torch as t
+import torch.nn as nn
+class myNet(nn.Module):
+    def __init__(self):
+        super(myNet, self).__init__()
+        self.conv1 = nn.Conv2d()
+
+    def train_net(self):
+        return 
+    def forward(self, x):
+        return x
