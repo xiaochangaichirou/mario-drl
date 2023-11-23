3315a3f5187ff561d06c7c51a9b1808a21043485
xiaochangaichirou <1207789049@qq.com>
2023-11-23 10:04:47 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -9,11 +9,10 @@ env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0')
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
 
 done = True
-print(device)
 for step in range(5000):
     if done:
         state = env.reset()
-    state, reward, done, info = env.step(env.action_space.sample())
+    state, reward, done, info = env.step(env.action_space.sample()) #state (240 256 3)
     time.sleep(0.01)
     env.render()
 
